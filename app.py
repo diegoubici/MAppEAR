59052467a7d76c5c884eae735d830a80807be64f
Diego Ubici <diegoubici@agropulso.com.ar>
2025-12-26 19:21:15 -0300

    Actualizacion automatica -12-2025 19:21

--- app.py
+++ app.py
@@ -62,7 +62,10 @@ USERS = {
     "usuario2": {"password": "contraseña2", "rol": "user"},
     "usuario3": {"password": "contraseña3", "rol": "user"},
     "usuario4": {"password": "contraseña4", "rol": "user"},
-    "RIVADAVIA": {"password": "rivadavia5", "rol": "user"},
+    "usuario5": {"password": "contraseña5", "rol": "user"},
+    "usuario6": {"password": "contraseña6", "rol": "user"},
+    "usuario7": {"password": "contraseña7", "rol": "user"},
+    "PELAYO": {"password": "pelayo", "rol": "user"},
 }
 
 # === UTILIDADES LOCALES ===
